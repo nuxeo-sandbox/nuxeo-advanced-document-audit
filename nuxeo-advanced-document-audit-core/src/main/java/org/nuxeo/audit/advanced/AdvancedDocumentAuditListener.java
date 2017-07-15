cbc08b8679fd3281b7db28848b64de05490be5af
Thibaud Arguillere <targuillere@nuxeo.com>
2017-07-15 15:18:57 -0400

    Fix stuff

--- nuxeo-advanced-document-audit-core/src/main/java/org/nuxeo/audit/advanced/AdvancedDocumentAuditListener.java
+++ nuxeo-advanced-document-audit-core/src/main/java/org/nuxeo/audit/advanced/AdvancedDocumentAuditListener.java
@@ -22,8 +22,6 @@ package org.nuxeo.audit.advanced;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.Blob;
-import org.nuxeo.ecm.core.api.DataModel;
-import org.nuxeo.ecm.core.api.DataModelMap;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.model.Property;
 import org.nuxeo.ecm.core.api.model.impl.ArrayProperty;
@@ -41,231 +39,216 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-
 public class AdvancedDocumentAuditListener implements EventListener {
 
-    private static final Log log = LogFactory.getLog(AdvancedDocumentAuditListener.class);
+	private static final Log log = LogFactory.getLog(AdvancedDocumentAuditListener.class);
 
-    private static final List<String> SYSTEM_PROPS = Arrays.asList(
-            "dc:created", "dc:creator", "dc:modified", "dc:contributors");
+	private static final List<String> SYSTEM_PROPS = Arrays.asList("dc:created", "dc:creator", "dc:modified",
+			"dc:contributors");
 
-    public static final String EVENT_ID = "Property Modification";
-    public static final String FIELD_NAME = "fieldname";
-    public static final String OLD_VALUE = "oldValue";
-    public static final String NEW_VALUE = "newValue";
-    public static final String EMPTY_VALUE = "EMPTY";
+	public static final String EVENT_ID = "Property Modification";
+	public static final String FIELD_NAME = "fieldname";
+	public static final String OLD_VALUE = "oldValue";
+	public static final String NEW_VALUE = "newValue";
+	public static final String EMPTY_VALUE = "EMPTY";
 
-    @Override
-    public void handleEvent(Event event) {
-        EventContext ectx = event.getContext();
-        if (!(ectx instanceof DocumentEventContext)) {
-            return;
-        }
+	@Override
+	public void handleEvent(Event event) {
+		EventContext ectx = event.getContext();
+		if (!(ectx instanceof DocumentEventContext)) {
+			return;
+		}
 
-        AuditLogger logger = Framework.getLocalService(AuditLogger.class);
-        if (logger == null) {
-            log.error("No AuditLogger implementation is available");
-            return;
-        }
+		AuditLogger logger = Framework.getLocalService(AuditLogger.class);
+		if (logger == null) {
+			log.error("No AuditLogger implementation is available");
+			return;
+		}
 
-        DocumentEventContext docCtx = (DocumentEventContext) ectx;
-        DocumentModel newDoc = docCtx.getSourceDocument();
+		DocumentEventContext docCtx = (DocumentEventContext) ectx;
+		DocumentModel newDoc = docCtx.getSourceDocument();
 
-        DocumentModel oldDoc = newDoc.getCoreSession().getDocument(newDoc.getRef());
+		DocumentModel oldDoc = newDoc.getCoreSession().getDocument(newDoc.getRef());
 
-        Context context = new Context(newDoc, oldDoc, event, logger);
-        processDocument(context);
-    }
+		Context context = new Context(newDoc, oldDoc, event, logger);
+		processDocument(context);
+	}
 
+	protected void processDocument(Context context) {
+		List<LogEntry> entries = new ArrayList<>();
 
-    protected void processDocument(Context context) {
-        List<LogEntry> entries = new ArrayList<>();
+		String[] schemas = context.newDoc.getSchemas();
+		for (String schema : schemas) {
+			Collection<Property> properties = context.newDoc.getPropertyObjects(schema);
+			for (Property property : properties) {
+				String fieldName = property.getName();
+				// skip system properties
+				if (SYSTEM_PROPS.contains(fieldName)) {
+					continue;
+				}
 
-        DataModelMap dataModels = context.newDoc.getDataModels();
-        for (DataModel model : dataModels.values()) {
-            if (model.isDirty()) {
-                entries.addAll(processModel(context, model));
-            }
-        }
+				if (property.isDirty()) {
+					Property oldProperty = context.oldDoc.getProperty(fieldName);
+					entries.addAll(processProperty(context, oldProperty, property));
+				}
+			}
+		}
 
-        if (entries.size() > 0) context.logger.addLogEntries(entries);
-    }
+		if (entries.size() > 0) {
+			context.logger.addLogEntries(entries);
+		}
+	}
 
+	protected List<LogEntry> processProperty(Context context, Property oldProperty, Property newProperty) {
 
-    protected List<LogEntry> processModel(Context context, DataModel newModel) {
+		List<LogEntry> entries = new ArrayList<>();
 
-        Collection<String> dirtyFieldNames = newModel.getDirtyFields();
-        List<LogEntry> entries = new ArrayList<>();
+		// Handle Scalar Properties
+		if (oldProperty.isScalar()) {
+			entries.add(processScalarProperty(context, oldProperty, newProperty));
+		}
 
-        for (String fieldName : dirtyFieldNames) {
-            //skip system properties
-            if (SYSTEM_PROPS.contains(fieldName)) continue;
+		// Handle Complex Properties
+		if (oldProperty.isComplex() && !oldProperty.isList()) {
+			if (oldProperty instanceof BlobProperty) {
+				entries.add(processBlobProperty(context, oldProperty, newProperty));
+			} else {
+				entries.addAll(processComplexProperty(context, oldProperty, newProperty));
+			}
+		}
 
-            Property oldProperty = context.oldDoc.getProperty(fieldName);
-            Property newProperty = context.newDoc.getProperty(fieldName);
-            entries.addAll(processProperty(context, oldProperty, newProperty));
-        }
+		// Handle Scalar List Properties
+		if (oldProperty instanceof ArrayProperty) {
+			entries.addAll(
+					processScalarList(context, oldProperty.getPath(), oldProperty.getValue(), newProperty.getValue()));
+		}
+		// org.nuxeo.ecm.core.api.model.Property
+		return entries;
+	}
 
-        return entries;
-    }
+	protected LogEntry processScalarProperty(Context context, Property oldProperty, Property newProperty) {
+		return getEntry(context, oldProperty.getPath(), oldProperty.getValue(), newProperty.getValue());
+	}
 
+	protected List<LogEntry> processScalarList(Context context, String fieldName, Object oldValue, Object newValue) {
+		List<LogEntry> entries = new ArrayList<>();
 
-    protected List<LogEntry> processProperty(Context context, Property oldProperty, Property newProperty) {
+		List<Serializable> oldList = null;
+		if (oldValue != null) {
+			oldList = Arrays.asList((Serializable[]) oldValue);
+		}
+		List<Serializable> newList = Arrays.asList((Serializable[]) newValue);
 
-        List<LogEntry> entries = new ArrayList<>();
+		fieldName = normalizeFieldName(fieldName);
 
-        //Handle Scalar Properties
-        if (oldProperty.isScalar()) {
-            entries.add(
-                    processScalarProperty(context, oldProperty, newProperty));
-        }
+		// get Added Values
+		List<Serializable> added = new ArrayList<>(newList);
+		if (oldList != null) {
+			added.removeAll(oldList);
+		}
+		for (Serializable addedValue : added) {
+			LogEntry entry = getEntry(context, fieldName, null, addedValue);
+			entry.setComment(fieldName + " : Added " + formatPropertyValue(addedValue));
+			entries.add(entry);
+		}
 
-        //Handle Complex Properties
-        if (oldProperty.isComplex() && !oldProperty.isList()) {
-            if (oldProperty instanceof BlobProperty) {
-                entries.add(processBlobProperty(context, oldProperty, newProperty));
-            } else {
-                entries.addAll(
-                        processComplexProperty(context, oldProperty, newProperty));
-            }
-        }
+		// get Removed Value
+		if (oldList != null) {
+			List<Serializable> removed = new ArrayList<>(oldList);
+			removed.removeAll(newList);
+			for (Serializable removedValue : removed) {
+				LogEntry entry = getEntry(context, fieldName, removedValue, null);
+				entry.setComment(fieldName + " : Removed " + formatPropertyValue(removedValue));
+				entries.add(entry);
+			}
+		}
 
-        //Handle Scalar List Properties
-        if (oldProperty instanceof ArrayProperty) {
-            entries.addAll(
-                    processScalarList(
-                            context, oldProperty.getPath(), oldProperty.getValue(), newProperty.getValue()));
-        }
+		return entries;
+	}
 
-        return entries;
-    }
+	protected List<LogEntry> processComplexProperty(Context context, Property oldProperty, Property newProperty) {
+		List<LogEntry> entries = new ArrayList<>();
+		Iterator<Property> dirtyProperties = newProperty.getDirtyChildren();
+		while (dirtyProperties.hasNext()) {
+			Property dirtyProperty = dirtyProperties.next();
+			entries.addAll(processProperty(context, oldProperty.get(dirtyProperty.getName()), dirtyProperty));
+		}
+		return entries;
+	}
 
+	protected LogEntry processBlobProperty(Context context, Property oldProperty, Property newProperty) {
+		Blob oldBlob = (Blob) oldProperty.getValue();
+		String oldFilename = oldBlob != null ? oldBlob.getFilename() : null;
+		Blob newBlob = (Blob) newProperty.getValue();
+		String newFilename = newBlob != null ? newBlob.getFilename() : null;
+		return getEntry(context, oldProperty.getPath(), oldFilename, newFilename);
+	}
 
-    protected LogEntry processScalarProperty(Context context, Property oldProperty, Property newProperty) {
-        return getEntry(context, oldProperty.getPath(), oldProperty.getValue(), newProperty.getValue());
-    }
+	protected LogEntry getEntry(Context context, String fieldName, Serializable oldValue, Serializable newValue) {
 
+		AuditLogger logger = context.logger;
+		Event event = context.event;
+		DocumentModel doc = context.newDoc;
 
-    protected List<LogEntry> processScalarList(Context context, String fieldName, Object oldValue, Object newValue) {
-        List<LogEntry> entries = new ArrayList<>();
+		LogEntry entry = logger.newLogEntry();
+		entry.setEventId(EVENT_ID);
+		entry.setCategory("Document");
+		entry.setEventDate(new Date(event.getTime()));
+		entry.setDocUUID(doc.getRef());
+		entry.setDocLifeCycle(doc.getCurrentLifeCycleState());
+		entry.setPrincipalName(doc.getCoreSession().getPrincipal().getName());
+		entry.setRepositoryId(doc.getRepositoryName());
 
-        List<Serializable> oldList = Arrays.asList((Serializable[]) oldValue);
-        List<Serializable> newList = Arrays.asList((Serializable[]) newValue);
+		fieldName = normalizeFieldName(fieldName);
 
-        fieldName = normalizeFieldName(fieldName);
+		Map<String, ExtendedInfo> extended = new HashMap<>();
+		extended.put(FIELD_NAME, logger.newExtendedInfo(fieldName));
 
-        //get Added Values
-        List<Serializable> added = new ArrayList<>(newList);
-        added.removeAll(oldList);
-        for (Serializable addedValue : added) {
-            LogEntry entry = getEntry(context, fieldName, null, addedValue);
-            entry.setComment(fieldName + " : Added " + formatPropertyValue(addedValue));
-            entries.add(entry);
-        }
+		String formatedOldValue = formatPropertyValue(oldValue);
+		String formatedNewValue = formatPropertyValue(newValue);
 
-        //get Removed Value
-        List<Serializable> removed = new ArrayList<>(oldList);
-        removed.removeAll(newList);
-        for (Serializable removedValue : removed) {
-            LogEntry entry = getEntry(context, fieldName, removedValue, null);
-            entry.setComment(fieldName + " : Removed " + formatPropertyValue(removedValue));
-            entries.add(entry);
-        }
+		extended.put(OLD_VALUE, logger.newExtendedInfo(formatedOldValue));
+		extended.put(NEW_VALUE, logger.newExtendedInfo(formatedNewValue));
+		entry.setExtendedInfos(extended);
 
-        return entries;
-    }
+		entry.setComment(fieldName + " : " + formatedOldValue + " -> " + formatedNewValue);
+		return entry;
+	}
 
+	protected String formatPropertyValue(Serializable value) {
+		if (value instanceof Calendar) {
+			Calendar calendar = (Calendar) value;
+			String pattern = "MM/dd/yyyy";
+			SimpleDateFormat format = new SimpleDateFormat(pattern);
+			return format.format(calendar.getTime());
+		} else if (value != null) {
+			return value.toString();
+		} else {
+			return EMPTY_VALUE;
+		}
+	}
 
-    protected List<LogEntry> processComplexProperty(Context context, Property oldProperty, Property newProperty) {
-        List<LogEntry> entries = new ArrayList<>();
-        Iterator<Property> dirtyProperties = newProperty.getDirtyChildren();
-        while (dirtyProperties.hasNext()) {
-            Property dirtyProperty = dirtyProperties.next();
-            entries.addAll(
-                    processProperty(context, oldProperty.get(dirtyProperty.getName()), dirtyProperty));
-        }
-        return entries;
-    }
+	protected String normalizeFieldName(String fieldName) {
+		if (fieldName.startsWith("/")) {
+			return fieldName.substring(1);
+		} else {
+			return fieldName;
+		}
+	}
 
+	class Context {
 
-    protected LogEntry processBlobProperty(Context context, Property oldProperty, Property newProperty) {
-        Blob oldBlob = (Blob) oldProperty.getValue();
-        String oldFilename = oldBlob != null ? oldBlob.getFilename() : null;
-        Blob newBlob = (Blob) newProperty.getValue();
-        String newFilename = newBlob != null ? newBlob.getFilename() : null;
-        return getEntry(context, oldProperty.getPath(), oldFilename, newFilename);
-    }
+		private DocumentModel newDoc;
+		private DocumentModel oldDoc;
+		private Event event;
+		private AuditLogger logger;
 
-
-    protected LogEntry getEntry(Context context, String fieldName, Serializable oldValue, Serializable newValue) {
-
-        AuditLogger logger = context.logger;
-        Event event = context.event;
-        DocumentModel doc = context.newDoc;
-
-        LogEntry entry = logger.newLogEntry();
-        entry.setEventId(EVENT_ID);
-        entry.setCategory("Document");
-        entry.setEventDate(new Date(event.getTime()));
-        entry.setDocUUID(doc.getRef());
-        entry.setDocLifeCycle(doc.getCurrentLifeCycleState());
-        entry.setPrincipalName(doc.getCoreSession().getPrincipal().getName());
-        entry.setRepositoryId(doc.getRepositoryName());
-
-        fieldName = normalizeFieldName(fieldName);
-
-        Map<String, ExtendedInfo> extended = new HashMap<>();
-        extended.put(FIELD_NAME, logger.newExtendedInfo(fieldName));
-
-        String formatedOldValue = formatPropertyValue(oldValue);
-        String formatedNewValue = formatPropertyValue(newValue);
-
-        extended.put(OLD_VALUE, logger.newExtendedInfo(formatedOldValue));
-        extended.put(NEW_VALUE, logger.newExtendedInfo(formatedNewValue));
-        entry.setExtendedInfos(extended);
-
-        entry.setComment(fieldName + " : " + formatedOldValue + " -> " + formatedNewValue);
-        return entry;
-    }
-
-
-    protected String formatPropertyValue(Serializable value) {
-        if (value instanceof Calendar) {
-            Calendar calendar = (Calendar) value;
-            String pattern = "MM/dd/yyyy";
-            SimpleDateFormat format = new SimpleDateFormat(pattern);
-            return format.format(calendar.getTime());
-        } else if (value != null) {
-            return value.toString();
-        } else {
-            return EMPTY_VALUE;
-        }
-    }
-
-
-    protected String normalizeFieldName(String fieldName) {
-        if (fieldName.startsWith("/")) {
-            return fieldName.substring(1);
-        } else {
-            return fieldName;
-        }
-    }
-
-
-    class Context {
-
-        private DocumentModel newDoc;
-        private DocumentModel oldDoc;
-        private Event event;
-        private AuditLogger logger;
-
-        public Context(DocumentModel newDoc, DocumentModel oldDoc, Event event, AuditLogger logger) {
-            this.newDoc = newDoc;
-            this.oldDoc = oldDoc;
-            this.event = event;
-            this.logger = logger;
-        }
-    }
-
+		public Context(DocumentModel newDoc, DocumentModel oldDoc, Event event, AuditLogger logger) {
+			this.newDoc = newDoc;
+			this.oldDoc = oldDoc;
+			this.event = event;
+			this.logger = logger;
+		}
+	}
 
 }
